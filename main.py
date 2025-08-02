c91b817ee16059c2f8acd48936d5b4213326b134
Andrew Beard <bearda@gmail.com>
2025-08-02 18:20:27 -0400

    Add a basic node info message

--- main.py
+++ main.py
@@ -4,12 +4,15 @@ import asyncio
 import aiomqtt
 
 from config import MQTTConfig
+from nodeinfomessage import MeshtasticNodeInfoMessage
 from textmessage import MeshtasticTextMessage
 
 
 async def main():
     config = MQTTConfig()
     async with aiomqtt.Client(**config.aiomqtt_config) as client:
+        message = MeshtasticNodeInfoMessage(config)
+        await client.publish(config.topic, bytes(message))
         message = MeshtasticTextMessage("Hello, world!", config)
         await client.publish(config.topic, bytes(message))
 
